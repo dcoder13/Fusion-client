06246f865493dbc83274ec9f7c48a750949bf13e
Mokshagnabelli <mokshagnabeli@gmail.com>
2024-11-19 16:19:05 +0530

    fix : edit budget ui

--- src/Modules/Iwd/components/ViewBudget.jsx
+++ src/Modules/Iwd/components/ViewBudget.jsx
@@ -1,33 +1,53 @@
 import React, { useState, useEffect } from "react";
-import { Table, Container, Paper, Title, Grid, Loader } from "@mantine/core";
+import { Container, Table, Title, Grid, Loader } from "@mantine/core";
 import { GetBudgets } from "../handlers/handlers";
 import "./GlobTable.css";
 
+// view budget fxn
 export default function ViewBudget() {
   const [loading, setLoading] = useState(false);
+
   const [ViewBudgetList, setBudgetList] = useState([]);
+
   useEffect(() => {
     GetBudgets({ setLoading, setBudgetList });
   }, []);
 
+  // const ViewBudgetList = [
+  //   {
+  //     id: "1",
+  //     name: "divyansh",
+  //     budgetIssued: 2000,
+  //   },
+  //   {
+  //     id: "2",
+  //     name: "user2",
+  //     budgetIssued: 2200,
+  //   },
+  //   {
+  //     id: "3",
+  //     name: "user3",
+  //     budgetIssued: 2100,
+  //   },
+  // ];
+
   return (
-    <Container className="container">
+    <Container className="container" style={{ padding: "10px" }}>
       <br />
+
       {loading ? (
         <Grid mt="xl">
           <Container py="xl">
             <Loader size="lg" />
           </Container>
         </Grid>
-      ) : (
-        <Paper className="s-table" shadow="xs" padding="md">
-          <div className="table-header">
-            <Title className="issue-work-order-button" size="h4">
-              Details
-            </Title>
-          </div>
-          <Table>
-            <thead>
+      ) : ViewBudgetList.length > 0 ? (
+        <div className="details-wrapper">
+          <Title align="center" mb="md" weight={700} size="26px">
+            Budget Details
+          </Title>
+          <Table highlightOnHover withBorder withColumnBorders>
+            <thead style={{ backgroundColor: "#f5f5f5" }}>
               <tr>
                 <th>ID</th>
                 <th>Name</th>
@@ -36,16 +56,17 @@ export default function ViewBudget() {
             </thead>
             <tbody>
               {ViewBudgetList.map((request, index) => (
-                <tr key={index} id={request.id}>
+                <tr key={index}>
                   <td>{request.id}</td>
                   <td>{request.name}</td>
-                  {/* <td>{request["budgetIssued"]}</td> */}
                   <td>{request.budgetIssued}</td>
                 </tr>
               ))}
             </tbody>
           </Table>
-        </Paper>
+        </div>
+      ) : (
+        console.log("No Data: Empty Array")
       )}
     </Container>
   );
