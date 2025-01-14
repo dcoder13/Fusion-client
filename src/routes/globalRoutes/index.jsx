49a7c784cdabd1ed1de8f263d415627bde21d902
Divyansh Bisht <115489768+dcoder13@users.noreply.github.com>
2025-01-14 18:49:27 +0530

    Update index.jsx

--- src/routes/globalRoutes/index.jsx
+++ src/routes/globalRoutes/index.jsx
@@ -1,3 +1,3 @@
-export const host = "http://127.0.0.1:8000";
+export const host = "https://fusion-gnff.onrender.com";
 export const authRoute = `${host}/api/auth/me`;
 export const loginRoute = `${host}/api/auth/login/`;
